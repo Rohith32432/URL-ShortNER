fd1cdac3e30e4e3fa7a299bcab0df0e05b93fe3b
Rohith_k <2100032432cse@gmail.com>
2024-06-04 22:37:36 +0530

    testing

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { customAlphabet } from 'nanoid';
 import  UrlModel from './model.js'
+import  dotenv from 'dotenv'
+dotenv.config()
 const server = express();
 server.set("view engine", "ejs");
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }));
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect(`mongodb://127.0.0.1:27017/test`);
+  await mongoose.connect(process.env.url);
   console.log('DB Connected successfully ');
 }
 server.get('/', async(req, res) => {
@@ -28,6 +30,10 @@ server.get('/:shorturl',async(req,res)=>{
     const url=await UrlModel.findOne({shorturl:req.params.shorturl})
     if(url==null) res.status(404).json('url not-found')
     res.redirect(url.fullurl)
+})
+server.get('/del/:id',async (req,res)=>{
+    await UrlModel.deleteOne({shorturl:req.params.id})
+    res.redirect('/')
 })
 
 server.listen(2239, () => {
